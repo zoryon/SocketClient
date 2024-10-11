bacd7038d355840b377b26e3b5416a042dd99c26
lai2-24 <lai2-24@itismeucci.com>
2024-10-11 08:51:55 +0200

    socket client

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,13 @@
 package com.example;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        System.out.println("Client partito");
+        Socket s = new Socket("localhost", 3000);
+        System.out.println("Il client si è collegato");
     }
 }
